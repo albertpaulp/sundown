998eca958859bfa8f8fa13c867bc79910ece1933
Albert <albert.paul@ruby-software.com>
2016-03-22 15:10:25 +0530

    Temperature in both C and F

--- spec/sundown_spec.rb
+++ spec/sundown_spec.rb
@@ -1,14 +1,27 @@
 require 'sundown'
 
 describe Sundown::Weather do
-  it 'Temp. in kodakara should be a float with using Weather class' do
-    expect(Sundown::Weather.temperature('680689')).to be_kind_of(Float)
+  it 'Temp.(Farenheit) in london should be a float' do
+    expect(Sundown::Weather.temperature('london')).to be_kind_of(Float)
+  end
+end
+
+describe Sundown::Weather do
+  it 'Temp.(Celcious) in london should be a float' do
+    expect(Sundown::Weather.temperature('london', 'c')).to be_kind_of(Float)
   end
 end
 
 describe Sundown::Location do
-  it 'Temp. in kodakara should be a float with using Location class' do
-    location = Sundown::Location.new('680689')
+  it 'Temp.(Farenheit) in london should be a float with using Location class' do
+    location = Sundown::Location.new('london')
     expect(location.temperature).to be_kind_of(Float)
+  end
+end
+
+describe Sundown::Location do
+  it 'Temp.(Celcious) in london should be a float with using Location class' do
+    location = Sundown::Location.new('london')
+    expect(location.temperature('c')).to be_kind_of(Float)
   end
 end
