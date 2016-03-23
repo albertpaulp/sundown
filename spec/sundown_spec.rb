3ba3ded773a190320e0f6786bf7652e257cf6e3c
Albert <albert.paul@ruby-software.com>
2016-03-23 21:53:34 +0530

    Timestamp added

--- spec/sundown_spec.rb
+++ spec/sundown_spec.rb
@@ -12,6 +12,10 @@ describe Sundown::Weather do
   it 'Temp.(Celcious) in london should be a float' do
     expect(Sundown::Weather.temperature('london', 'c')).to be_kind_of(Float)
   end
+
+  it 'Timestamp of london object should be a string' do
+    expect(Sundown::Weather.timestamp('london')).to be_kind_of(String)
+  end
 end
 
 describe Sundown::Location do
@@ -25,9 +29,14 @@ describe Sundown::Location do
     expect(location.temperature('c')).to be_kind_of(Float)
   end
 
-  it 'Refresh on Location should return nil on @weather' do
+  it 'Refresh on Location should return nil' do
     location = Sundown::Location.new('london')
     expect(location.refresh).to eql(nil)
+  end
+
+  it 'Timestamp on Location should return string' do
+    location = Sundown::Location.new('london')
+    expect(location.timestamp).to be_kind_of(String)
   end
 end
 
